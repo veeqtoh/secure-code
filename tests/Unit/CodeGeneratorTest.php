61348179be57cf86c4a5f559384f923fe46d5d70
Victor Ukam <victorjohnukam@gmail.com>
2023-08-04 21:16:11 +0100

    Setting up Unit Tests

---
+++ tests/Unit/CodeGeneratorTest.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace Tests\Unit;
+
+use PHPUnit\Framework\TestCase;
+
+class CodeGeneratorTest extends TestCase
+{
+    /**
+     * A basic unit test example.
+     */
+    public function test_example(): void
+    {
+        $this->assertTrue(true);
+    }
+}

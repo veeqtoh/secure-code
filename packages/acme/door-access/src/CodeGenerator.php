7b1525f36295110162bc97b9e0bf5daf02c7dc79
Victor Ukam <victorjohnukam@gmail.com>
2023-08-04 21:19:18 +0100

    Quick typo fix

--- packages/acme/door-access/src/CodeGenerator.php
+++ packages/acme/door-access/src/CodeGenerator.php
@@ -1,5 +1,5 @@
 <?php
-declare(strict_type=1);
+declare(strict_types=1);
 
 namespace Veeqtoh\DoorAccess;
 
--- tests/Unit/CodeGeneratorTest.php
+++ tests/Unit/CodeGeneratorTest.php
@@ -1,8 +1,10 @@
 <?php
+declare(strict_types=1);
 
 namespace Tests\Unit;
 
 use PHPUnit\Framework\TestCase;
+use Veeqtoh\DoorAccess\CodeGenerator;
 
 class CodeGeneratorTest extends TestCase
 {

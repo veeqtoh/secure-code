7e4e9c88f103a20fd11bc571dd9255a32bdd0a5e
Victor Ukam <victorjohnukam@gmail.com>
2023-08-04 20:53:27 +0100

    Meeting requirements and constraints for code generation

--- packages/acme/door-access/src/CodeGenerator.php
+++ packages/acme/door-access/src/CodeGenerator.php
@@ -3,8 +3,6 @@ declare(strict_type=1);
 
 namespace Veeqtoh\DoorAccess;
 
-use Ramsey\Uuid\Uuid;
-
 class CodeGenerator
 {
     /**
@@ -14,8 +12,80 @@ class CodeGenerator
      */
     public function generateCode(): string
     {
-        $uuid = Uuid::uuid4();
-        $code = preg_replace('/[^0-9]/', '', $uuid->toString());
-        return substr($code, 0, 6);
+        do {
+            $code = $this->generateRandomCode();
+        } while (!$this->isCodeValid($code));
+
+        return $code;
+    }
+
+    /**
+     * Generate a random 6-digit code.
+     *
+     * @return string
+     */
+    private function generateRandomCode(): string
+    {
+        return (string) random_int(100000, 999999);
+    }
+
+    /**
+     * Check if the generated code is valid according to the constraints.
+     *
+     * @param string $code
+     * @return bool
+     */
+    private function isCodeValid(string $code): bool
+    {
+        return (
+            $this->isNotPalindrome($code) &&
+            $this->hasNoCharacterRepeatedMoreThanThreeTimes($code) &&
+            $this->hasNoSequenceLengthGreaterThanThree($code) &&
+            $this->hasAtLeastThreeUniqueCharacters($code)
+        );
+    }
+
+    /**
+     * Check if the code is not a palindrome.
+     *
+     * @param string $code
+     * @return bool
+     */
+    private function isNotPalindrome(string $code): bool
+    {
+        return $code !== strrev($code);
+    }
+
+    /**
+     * Check if the code has no character repeated more than three times.
+     *
+     * @param string $code
+     * @return bool
+     */
+    private function hasNoCharacterRepeatedMoreThanThreeTimes(string $code): bool
+    {
+        return !preg_match('/(\d)\1{3,}/', $code);
+    }
+
+    /**
+     * Check if the code has no sequence length greater than three.
+     *
+     * @param string $code
+     * @return bool
+     */
+    private function hasNoSequenceLengthGreaterThanThree(string $code): bool
+    {
+        return !preg_match('/(\d)\1{2,}/', $code);
+    }
+
+    /**
+     * Check if the code has at least three unique characters.
+     *
+     * @param string $code
+     * @return bool
+     */
+    private function hasAtLeastThreeUniqueCharacters(string $code): bool
+    {
+        return count(array_count_values(str_split($code))) >= 3;
     }
 }

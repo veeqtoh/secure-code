cbef0d7f7d054df977863af624f71d00853fd67f
Victor Ukam <victorjohnukam@gmail.com>
2023-08-04 23:10:14 +0100

    Updated CodeGenerator to be more Scalable

--- src/CodeGenerator.php
+++ src/CodeGenerator.php
@@ -1,4 +1,5 @@
 <?php
+
 declare(strict_types=1);
 
 namespace Veeqtoh\DoorAccess;
@@ -8,13 +9,14 @@ class CodeGenerator
     /**
      * Generate a unique 6-digit code.
      *
-     * @return string
+     * @param array $rules The rules for generating the code.
+     * @return string|null The generated code or null if code generation fails.
      */
-    public function generateCode(): string
+    public function generateCode(array $rules): ?string
     {
         do {
             $code = $this->generateRandomCode();
-        } while (!$this->isCodeValid($code));
+        } while (!$this->isCodeValid($code, $rules));
 
         return $code;
     }
@@ -33,15 +35,21 @@ class CodeGenerator
      * Check if the generated code is valid according to the constraints.
      *
      * @param string $code
+     * @param array $rules The rules for generating the code.
      * @return bool
      */
-    private function isCodeValid(string $code): bool
+    private function isCodeValid(string $code, array $rules): bool
     {
+        $allowedCharacters = $rules['allowed_characters'] ?? '0123456789';
+        $characterRepeatedLimit = $rules['character_repeated_limit'] ?? 3;
+        $sequenceLengthLimit = $rules['sequence_length_limit'] ?? 3;
+        $uniqueCharactersLimit = $rules['unique_characters_limit'] ?? 3;
+
         return (
             $this->isNotPalindrome($code) &&
-            $this->hasNoCharacterRepeatedMoreThanThreeTimes($code) &&
-            $this->hasNoSequenceLengthGreaterThanThree($code) &&
-            $this->hasAtLeastThreeUniqueCharacters($code)
+            $this->hasNoCharacterRepeatedMoreThanLimit($code, $characterRepeatedLimit) &&
+            $this->hasNoSequenceLengthGreaterThanLimit($code, $sequenceLengthLimit) &&
+            $this->hasAtLeastLimitUniqueCharacters($code, $uniqueCharactersLimit, $allowedCharacters)
         );
     }
 
@@ -57,35 +65,49 @@ class CodeGenerator
     }
 
     /**
-     * Check if the code has no character repeated more than three times.
+     * Check if the code has no character repeated more than the limit.
      *
      * @param string $code
+     * @param int $limit
      * @return bool
      */
-    private function hasNoCharacterRepeatedMoreThanThreeTimes(string $code): bool
+    private function hasNoCharacterRepeatedMoreThanLimit(string $code, int $limit): bool
     {
-        return !preg_match('/(\d)\1{3,}/', $code);
+        return !preg_match('/(\d)\1{' . ($limit - 1) . ',}/', $code);
     }
 
     /**
-     * Check if the code has no sequence length greater than three.
+     * Check if the code has no sequence length greater than the limit.
      *
      * @param string $code
+     * @param int $limit
      * @return bool
      */
-    private function hasNoSequenceLengthGreaterThanThree(string $code): bool
+    private function hasNoSequenceLengthGreaterThanLimit(string $code, int $limit): bool
     {
-        return !preg_match('/(\d)\1{2,}/', $code);
+        return !preg_match('/(\d)\1{' . ($limit - 1) . ',}/', $code);
     }
 
     /**
-     * Check if the code has at least three unique characters.
+     * Check if the code has at least the limit number of unique characters from the allowed characters.
      *
      * @param string $code
+     * @param int $limit
+     * @param string $allowedCharacters
      * @return bool
      */
-    private function hasAtLeastThreeUniqueCharacters(string $code): bool
+    private function hasAtLeastLimitUniqueCharacters(string $code, int $limit, string $allowedCharacters): bool
     {
-        return count(array_count_values(str_split($code))) >= 3;
+        $allowedCharactersCount = count_chars($allowedCharacters, 1);
+        $codeCharactersCount = count_chars($code, 1);
+
+        $uniqueCharactersCount = 0;
+        foreach ($codeCharactersCount as $character => $count) {
+            if (isset($allowedCharactersCount[$character])) {
+                $uniqueCharactersCount++;
+            }
+        }
+
+        return $uniqueCharactersCount >= $limit;
     }
 }

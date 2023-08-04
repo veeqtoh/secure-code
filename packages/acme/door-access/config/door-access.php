d9eb10849620afc763d6abb31582e0ca94cdfa7a
Victor Ukam <victorjohnukam@gmail.com>
2023-08-04 02:19:17 +0100

    Setting up config

---
+++ packages/acme/door-access/config/door-access.php
@@ -0,0 +1,15 @@
+<?php
+
+return [
+    /*
+    |--------------------------------------------------------------------------
+    | Door Access Configuration
+    |--------------------------------------------------------------------------
+    |
+    | This file is for configuring the door access system package.
+    |
+    */
+
+    'code_length' => 6,
+    'unique_characters' => '0123456789',
+];

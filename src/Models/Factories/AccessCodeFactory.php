ef497849a00178a52bfdaaf3183fc22527091a1d
Victor Ukam <victorjohnukam@gmail.com>
2024-04-30 20:14:35 +0100

    Setup model and factory

---
+++ src/Models/Factories/AccessCodeFactory.php
@@ -0,0 +1,21 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Veeqtoh\DoorAccess\Models\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+use Veeqtoh\DoorAccess\Models\AccessCode;
+
+/**
+ * @extends Factory<AccessCode>
+ */
+class AccessCodeFactory extends Factory
+{
+    protected $model = AccessCode::class;
+
+    public function definition()
+    {
+    }
+
+}

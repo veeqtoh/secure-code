6a7e36cf9aff3ebd8fd22ef032d27fd9f68d5cb5
Victor Ukam <victorjohnukam@gmail.com>
2024-04-30 21:44:14 +0100

    Fix psr-4 standard issues

--- src/Classes/Validators/NoPalindromeValidator.php
+++ src/Classes/Validators/NoPalindromeValidator.php
@@ -4,7 +4,7 @@ declare(strict_types=1);
 
 namespace Veeqtoh\DoorAccess\Classes\Validators;
 
-use Veeqtoh\DoorAccess\Interfaces\CodeValidator;
+use Veeqtoh\DoorAccess\Contracts\CodeValidator;
 
 /**
  * Class NoPalindromeValidator
--- src/Contracts/CodeValidator.php
+++ src/Contracts/CodeValidator.php
@@ -2,10 +2,10 @@
 
 declare(strict_types=1);
 
-namespace Veeqtoh\DoorAccess\Interfaces;
+namespace Veeqtoh\DoorAccess\Contracts;
 
 /**
- * Interface CodeValidationRule
+ * Interface CodeValidator
  * @package Veeqtoh\DoorAccess\Contracts
  */
 interface CodeValidator

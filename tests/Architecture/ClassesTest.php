970d9109cd6f012156fb292501d57edde2df3338
Victor Ukam <victorjohnukam@gmail.com>
2024-05-01 23:16:05 +0100

    Add more architectural test

--- tests/Architecture/ClassesTest.php
+++ tests/Architecture/ClassesTest.php
@@ -1,5 +1,6 @@
 <?php
 
+use Veeqtoh\DoorAccess\Classes\Traits\ConfigTrait;
 use Veeqtoh\DoorAccess\Contracts\CodeValidator;
 
 test('minimum unique characters validator implements the validator interface')
@@ -16,3 +17,28 @@ test('repeating characters validator implements the validator interface')
     ->expect('Veeqtoh\DoorAccess\Classes\Validators\RepeatingCharactersValidator')
     ->classes()
     ->toImplement(CodeValidator::class);
+
+test('config trait is a trait')
+    ->expect('Veeqtoh\DoorAccess\Classes\Traits\ConfigTrait')
+    ->classes()
+    ->toBeTraits();
+
+test('code generator uses config trait')
+    ->expect('Veeqtoh\DoorAccess\Classes\CodeGenerator')
+    ->traits()
+    ->toExtend(ConfigTrait::class);
+
+test('minimum unique characters validator uses config trait')
+    ->expect('Veeqtoh\DoorAccess\Classes\Validators\MinimumUniqueCharactersValidator')
+    ->traits()
+    ->toExtend(ConfigTrait::class);
+
+test('no palindrome validator validator uses config trait')
+    ->expect('Veeqtoh\DoorAccess\Classes\Validators\NoPalindromeValidator')
+    ->traits()
+    ->toExtend(ConfigTrait::class);
+
+test('repeating characters validator validator uses config trait')
+    ->expect('Veeqtoh\DoorAccess\Classes\Validators\RepeatingCharactersValidator')
+    ->traits()
+    ->toExtend(ConfigTrait::class);

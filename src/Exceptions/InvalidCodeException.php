a5a146fd5b4382f2ded138401455206d51d17a04
Victor Ukam <victorjohnukam@gmail.com>
2024-05-04 00:38:08 +0100

    Setup invalid code exception

---
+++ src/Exceptions/InvalidCodeException.php
@@ -0,0 +1,18 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Veeqtoh\DoorAccess\Exceptions;
+
+use Exception;
+
+/**
+ * class InvalidCodeException
+ * This class provides the exception for library config validations.
+ *
+ * @package Veeqtoh\DoorAccess\Exceptions
+ */
+class InvalidCodeException extends Exception
+{
+    //
+}
